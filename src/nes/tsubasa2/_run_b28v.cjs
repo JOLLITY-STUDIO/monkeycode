4f7fe3904e727f668052dc5b901cc725769cb539
Jason Hong <jollitycn@foxmail.com>
2026-08-21 07:34:45 +0800

    清理误纳入仓库的临时诊断文件(从索引移除, 保留工作区)

--- src/nes/tsubasa2/_run_b28v.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const s = fs.readFileSync('src/game/data/prg/DataStore.ts', 'utf8');
-const lines = s.split('\n');
-for (let i = 0; i < lines.length; i++) {
-  if (/isBusy|beginBuild|endBuild|setBusy|class Oam|busy/.test(lines[i])) {
-    console.log((i + 1) + ': ' + lines[i]);
-  }
-}
