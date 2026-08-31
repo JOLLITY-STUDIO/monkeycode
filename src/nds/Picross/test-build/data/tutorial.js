52da82d92e577b9a59ee9ebe33d601d1fa9532cf
Jason Hong <jollitycn@foxmail.com>
2026-08-31 23:04:54 +0800

    feat(nds): 引入 EssentialSudokuDS 数独项目 (V0.15, 1000 题库) 至父仓库，移除 Picross 项目文件

--- src/nds/Picross/test-build/data/tutorial.js
+++
@@ -1,9 +0,0 @@
-export const TUTORIAL_PUZZLE = {
-    id: -1,
-    name: "How to Play",
-    width: 5,
-    height: 5,
-    difficulty: 0, // Tutorial
-    unlocked: true,
-    solutionHex: "EFFFF800",
-};
